d0955a04178f33ee90e87f1435cc6cde0c7daa6b
Dev Ashish <ashish.nanda89@outlook.com>
2025-05-11 21:44:30 +0530

    Catalog Api Infrastructure code commit

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,8 +1,34 @@
 ﻿namespace Catalog.API.Products.CreateProduct
 {
-    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler
+    internal class CreateProductCommandHandler(IDocumentSession session) 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // Create Product entity from command object
+            //save to database
+            //return CreateProductResult result
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            //TODO
+            //save to database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            //return result
+
+            return new CreateProductResult(product.Id);
+        }
     }
 }
